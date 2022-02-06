9cd776d48ca56d554540ba1c06abe8e04cb6ad92
padma2510 <padmapadma1061@gmail.com>
2022-02-05 20:09:34 -0800

    hi

--- src/org/cts/Client.java
+++ src/org/cts/Client.java
@@ -1,5 +1,14 @@
 package org.cts;
 
 public class Client {
+	public void sub() {
+	System.out.println("sub");	// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		Client c = new Client();
+		c.sub();
+		
+	}
 
 }
